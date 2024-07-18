d416610a4df721cdb84523d0f4d4f79c61802ae2
Anand Kumar Singh <anandkumarsingh0615@gmail.com>
2024-07-19 03:41:47 +0530

    Initial commit

---
+++ src/components/WorkCardData.jsx
@@ -0,0 +1,28 @@
+import pro1 from "../assets/project1.jpg"
+import pro2 from "../assets/project1.jpg"
+import pro3 from "../assets/project1.jpg"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded,tangible and self-affirming sense of Wow I really can do this for over 100,000 online entrepreneurs. Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+    },
+
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded,tangible and self-affirming sense of Wow I really can do this for over 100,000 online entrepreneurs. Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+    },
+
+    {
+        imgsrc: pro3,
+        title: "Rymo Ecommerce Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded,tangible and self-affirming sense of Wow I really can do this for over 100,000 online entrepreneurs. Best-selling marketing courses",
+        view: "https://www.youtube.com/watch?v=P8YuWEkTeuE"
+    },
+];
+
+export default ProjectCardData
